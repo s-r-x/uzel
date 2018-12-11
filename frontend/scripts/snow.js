828bc1494f28834275c2c4cd644ff06003c23d98
s-r-x <strax1990@gmail.com>
2018-12-11 11:10:16 +0500

    remove degugging info

--- frontend/scripts/snow.js
+++ frontend/scripts/snow.js
@@ -2,7 +2,6 @@ import 'particles.js';
 import config from './particles-config.json';
 
 window.letItSnow = (elId) => {
-  console.log(config);
   config.particles.shape.image.src = window.__snow__.snowflake_url;
   particlesJS(elId, config);
 }
