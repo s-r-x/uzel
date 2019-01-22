2604fe23c7f13f0007eeb65f3a6795de32795816
s-r-x <strax1990@gmail.com>
2019-01-22 17:25:58 +0500

    reveal add load event/dark theme header color fixes

--- frontend/scripts/reveal.js
+++ frontend/scripts/reveal.js
@@ -5,31 +5,33 @@ function isMobile(agent = navigator.userAgent) {
 }
 function run() {
   const qsa = document.querySelectorAll.bind(document);
-  const instance = Reveal();
-  const config = {
-    viewFactor: 0.3,
-    duration: 1000,
-    easing: 'ease-in-out',
-  }
-  const thumbConfig = {
-    cleanup: true,
-    viewFactor: 0.3,
-    duration: 1400,
-    easing: 'ease-in-out',
-    beforeReveal(el) {
-      $(el).addClass('no-reveal');
+  window.addEventListener("load", function() {
+    const instance = Reveal();
+    const config = {
+      viewFactor: 0.3,
+      duration: 1000,
+      easing: 'ease-in-out',
     }
-  };
+    const thumbConfig = {
+      cleanup: true,
+      viewFactor: 0.3,
+      duration: 1400,
+      easing: 'ease-in-out',
+      beforeReveal(el) {
+        $(el).addClass('no-reveal');
+      }
+    };
 
-  const $thumbs = qsa('.post-item .post-thumbnail');
-  const $singleImgs = qsa('article.single-post img');
+    const $thumbs = qsa('.post-item .post-thumbnail');
+    const $singleImgs = qsa('article.single-post img');
 
-  window.revealStuff = true;
-  window.revealInstance = instance;
-  window.revealConfig = thumbConfig;
+    window.revealStuff = true;
+    window.revealInstance = instance;
+    window.revealConfig = thumbConfig;
 
-  instance.reveal($thumbs, thumbConfig);
-  instance.reveal($singleImgs, config);
+    instance.reveal($thumbs, thumbConfig);
+    instance.reveal($singleImgs, config);
+  });
 }
 
 if(!isMobile()) {
